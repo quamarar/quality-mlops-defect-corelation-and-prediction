dcef9f322dcbc67b874b887bb73cfc30a57afd5f
quamarar <122767728+quamarar@users.noreply.github.com>
2023-06-22 13:56:11 +0530

    Update OrchestratorPipeline.groovy

--- jenkins/seed-jobs/OrchestratorPipeline.groovy
+++ jenkins/seed-jobs/OrchestratorPipeline.groovy
@@ -1,1 +1,14 @@
-
+pipelineJob('github-demo') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('quamarar/quality-mlops-defect-corelation-and-prediction')
+                    }
+                }
+            }
+            scriptPath('jenkins/pipelines/Orchestrator.Jenkinsfile')
+        }
+    }
+}
