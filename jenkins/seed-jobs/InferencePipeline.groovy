fd41b96fd4b78bc39f7fb4ddea8716c33a0c8756
quamarar <122767728+quamarar@users.noreply.github.com>
2023-06-22 13:56:58 +0530

    Update InferencePipeline.groovy

--- jenkins/seed-jobs/InferencePipeline.groovy
+++ jenkins/seed-jobs/InferencePipeline.groovy
@@ -1,1 +1,15 @@
+pipelineJob('github-demo') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('quamarar/quality-mlops-defect-corelation-and-prediction')
+                    }
+                }
+            }
+            scriptPath('jenkins/pipelines/Inference.Jenkinsfile')
+        }
+    }
+}
 
