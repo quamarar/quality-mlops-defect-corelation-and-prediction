b21bc3409b6973977bb833c317c9ce169c76b1b1
quamarar <122767728+quamarar@users.noreply.github.com>
2023-06-28 10:06:46 +0530

    Update OrchestratorPipeline.groovy

--- jenkins/seed-jobs/OrchestratorPipeline.groovy
+++ jenkins/seed-jobs/OrchestratorPipeline.groovy
@@ -1,4 +1,8 @@
 pipelineJob('seed-jobs-MSIL-Orchestrator') {
+
+    parameters {
+         stringParam('Folder','Orchestrator','name of the folder')
+        }
     definition {
         cpsScm {
             scm {
