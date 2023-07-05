c414a672502b9ba5db8c9fef3363c2d3473b7afa
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-05 12:02:31 +0530

    Update InfraPipeline.groovy

--- jenkins/seed-jobs/InfraPipeline.groovy
+++ jenkins/seed-jobs/InfraPipeline.groovy
@@ -1,7 +1,7 @@
 pipelineJob('seed-jobs-MSIL-Infra') {
 
   parameters {
-    stringParam('AWS_REGION', 'aws region', 'us-east-1',)
+    stringParam('AWS_REGION','us-east-1','aws region')
     
     choiceParam('action', ['plan', 'apply', 'show', 'preview-destroy', 'destroy'], 'Terraform action to apply')
     choiceParam('ENVIRONMENT', ['dev', 'uat', 'prod'], 'deployment environment',)
