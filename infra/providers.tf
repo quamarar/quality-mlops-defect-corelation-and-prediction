3866c0c9535ca3e9cb4135b9afa5b2995d1da3d9
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-09 13:22:52 +0530

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.15, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.61.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 0.49.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.Deployment_Region
+}
