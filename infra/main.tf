440a9a88359e225ca783a12fd12dddd299e3982e
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-17 18:27:44 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -5,8 +5,6 @@
 module "kms_default" {
   source        = "git::https://github.com/quamarar/terraform-common-module.git?ref=master"
 
-  aliases       = [
-    "${local.name_prefix}-default-key"
-  ]
+
 
 }
