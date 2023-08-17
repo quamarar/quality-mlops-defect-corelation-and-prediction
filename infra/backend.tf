ad7f3e81e004a984d7bbb828482678de47296d96
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-17 12:25:50 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "shared-298320596430"
-    region         = "us-east-1"
+    bucket         = "tfstate"
+    region         = "ap-south-1"
     encrypt        = true
   }
 }
