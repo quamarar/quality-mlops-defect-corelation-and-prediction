668365ee4e4696a579154bb83508a2cb3cc25c58
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-17 17:45:11 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,15 @@
+/*===============================
+#       Common Details
+===============================*/
+
+variable "application-name" {
+  type        = string
+  description = "Application identifier"
+}
+
+variable "stage-name" {
+  type        = string
+  description = "Stage / Environment identifier"
+}
+
 
