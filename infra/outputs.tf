0532c629f24e6193fb9f21013590e64df2a23fa4
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-17 12:45:31 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,19 +1,1 @@
-/*===============================
-#              S3
-===============================*/
 
-output "logging-s3-bucket" {
-  value       = module.logging-s3-bucket.s3_bucket_id
-}
-
-output "training-s3-bucket" {
-  value       = module.training-s3-bucket.s3_bucket_id
-}
-
-output "inference-s3-bucket" {
-  value       = module.inference-s3-bucket.s3_bucket_id
-}
-
-output "monitoring-s3-bucket" {
-  value       = module.monitoring-s3-bucket.s3_bucket_id
-}
