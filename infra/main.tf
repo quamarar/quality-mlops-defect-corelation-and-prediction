0ceef9891c9eb094fd2ee7215f3609c52986cd11
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-21 15:30:52 +0530

    chalo

--- infra/main.tf
+++ infra/main.tf
@@ -282,32 +282,6 @@ module "ecr_preprocessing" {
   repository_force_delete = true
 }
 
-module "ecr_training" {
-  source = "git::https://github.com/quamarar/terraform-common-module.git//terraform-aws-ecr?ref=master"
-
-  repository_name = "${local.name_prefix}-${var.training-private-ecr-config.repository_name}"
-
-  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-  create_lifecycle_policy           = true
-  repository_lifecycle_policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1,
-        description  = "Keep last 30 images",
-        selection = {
-          tagStatus     = "tagged",
-          tagPrefixList = ["v"],
-          countType     = var.training-private-ecr-config.countType,
-          countNumber   = 30
-        },
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-  repository_force_delete = true
-}
 
 /*===============================
 #     ECR Registry Management
@@ -342,6 +316,40 @@ module "ecr_registry_processing" {
       }
   ]
 }
+
+/*===============================
+#           ECR
+===============================*/
+module "ecr_training" {
+  source = "git::https://github.com/quamarar/terraform-common-module.git//terraform-aws-ecr?ref=master"
+
+  repository_name = "${local.name_prefix}-${var.training-private-ecr-config.repository_name}"
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  create_lifecycle_policy           = true
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = var.training-private-ecr-config.countType,
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+  repository_force_delete = true
+}
+
+/*===============================
+#     ECR Registry Management
+===============================*/
 
 module "ecr_registry_training" {
   source = "git::https://github.com/quamarar/terraform-common-module.git//terraform-aws-ecr?ref=master"
