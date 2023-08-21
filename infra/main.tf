1732c95cc47a51ec2b746a3a2bb13fde878c0814
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-21 12:12:06 +0530

    yes

--- infra/main.tf
+++ infra/main.tf
@@ -3,8 +3,10 @@
 ===============================*/
 
 module "kms_default" {
-  source        = "git::https://github.com/quamarar/terraform-common-module.git?ref=master"
+  source        = "git::https://github.com/quamarar/terraform-common-module.git///terraform-aws-kms-master?ref=master"
 
-
+  aliases       = [
+    "${local.name_prefix}-default-key"
+  ]
 
 }
