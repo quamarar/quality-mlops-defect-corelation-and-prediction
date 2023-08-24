f3022f40382db7c80c068c5e0b6622546a127b44
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-24 11:45:05 +0530

    y

--- infra/main.tf
+++ infra/main.tf
@@ -27,26 +27,7 @@ module "shared-s3-bucket" {
   source = "git::https://github.com/quamarar/terraform-common-module.git//s3-bucket?ref=master"
 
   bucket                                = "${local.name_prefix}-${var.shared-s3-config.name}"
-  force_destroy                         = true
-  attach_policy                         = var.shared-s3-config.attach_policy
-  policy                                = null
-  attach_deny_insecure_transport_policy = var.shared-s3-config.attach_deny_insecure_transport_policy
-  attach_require_latest_tls_policy      = var.shared-s3-config.attach_require_latest_tls_policy
-  control_object_ownership              = var.shared-s3-config.control_object_ownership
   expected_bucket_owner                 = data.aws_caller_identity.current.account_id
-  acl                                   = var.shared-s3-config.acl  
-  versioning  = {
-    status                    = var.shared-s3-config.enable_versioning
-    mfa_delete                = var.shared-s3-config.enable_mfa_delete
-  }
-  server_side_encryption_configuration = {
-    rule      = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id     =  module.kms_default.key_arn
-        sse_algorithm         =  "aws:kms"
-      }
-    }
-  }
 }
 
 /*===============================
