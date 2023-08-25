b96d10215bc7b87e3fe5dbc22851fda1a932b64b
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-25 11:38:00 +0530

    y

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ module "kms_default" {
 ===============================*/
 
 module "internal-s3-bucket" {
-  source = "git::https://github.com/quamarar/terraform-common-module.git//terraform-aws-s3-bucket/?ref=master"
+  source = "git::https://github.com/quamarar/terraform-common-module.git//s3-bucket/?ref=master"
 
   bucket                               = "${local.name_prefix}-${var.internal-s3-config.name}"
   expected_bucket_owner                 =   data.aws_caller_identity.current.account_id
